97d872c965029c750aca0bb943159d8ed21603be
Arakakig <guipecoisarakaki@gmail.com>
2022-03-14 14:40:51 -0300

    app routing isn't works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ListComponent } from './usage-notes/list.component';
 
 const routes: Routes = [
   {path: '', component: AppComponent},
-  {path: 'notes', component: ListComponent},
+  {path: 'teste', component: AppComponent},
 ];
 
 @NgModule({
